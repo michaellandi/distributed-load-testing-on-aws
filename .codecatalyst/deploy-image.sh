5a84ac9ee047141d3ca480d686394a27d5383f4f
Michael Landi <mlandi.design@gmail.com>
2024-06-21 12:38:10 -0400

    Update deploy-image.sh

--- .codecatalyst/deploy-image.sh
+++ .codecatalyst/deploy-image.sh
@@ -3,17 +3,17 @@
 set -e
 
 if [ -z "$PUBLIC_ECR_REGISTRY" ]; then
-    echo "public.ecr.aws/aws-solutions";
-    return 0;
+    echo "Skipping image deployment...";
 elif [[ "$PUBLIC_ECR_REGISTRY" = "public.ecr.aws/aws-solutions" ]]; then
-    echo "public.ecr.aws/aws-solutions";
-    return 0;
+    echo "Skipping image deployment...";
+else
+    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$PUBLIC_ECR_REGISTRY"
+    
+    cd deployment/ecr/distributed-load-testing-on-aws-load-tester
+    
+    docker build -t distributed-load-testing-on-aws-load-tester .
+    docker tag distributed-load-testing-on-aws-load-tester:latest "$PUBLIC_ECR_REGISTRY:latest"
+    docker push "$PUBLIC_ECR_REGISTRY:latest"
 fi
 
-aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$PUBLIC_ECR_REGISTRY"
 
-cd deployment/ecr/distributed-load-testing-on-aws-load-tester
-
-docker build -t distributed-load-testing-on-aws-load-tester .
-docker tag distributed-load-testing-on-aws-load-tester:latest "$PUBLIC_ECR_REGISTRY:latest"
-docker push "$PUBLIC_ECR_REGISTRY:latest"
